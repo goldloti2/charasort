82cff2d25a356c00ad1a7fffd48ad789e64339a8
goldloti2 <goldloti2@gmail.com>
2025-05-31 15:58:25 +0800

    setup fliter, add 1.0 characters

--- src/js/data/2025-05-31.js
+++ src/js/data/2025-05-31.js
@@ -3,84 +3,272 @@ dataSet[dataSetVersion] = {};
 
 dataSet[dataSetVersion].options = [
   {
-    name: "Filter by Series Entry",
-    key: "series",
-    tooltip: "Check this to restrict to certain series.",
+    name: "Filter by Type",
+    key: "type",
+    tooltip: "Check this to restrict to characters attack type.",
     checked: false,
     sub: [
-      { name: "Books and CDs", key: "book" },
-      { name: "The Highly Responsive to Prayers", tooltip: "01 - Reiiden", key: "HRtP" },
-      { name: "The Story of Eastern Wonderland", tooltip: "02 - Fuumaroku", key: "SoEW" },
-      { name: "Phantasmagoria of Dim.Dream", tooltip: "03 - Yumejikuu", key: "PoDD" },
-      { name: "Lotus Land Story", tooltip: "04 - Gensoukyou", key: "LLS" },
-      { name: "Mystic Square", tooltip: "05 - Kaikidan", key: "MS" },
-      { name: "Embodiment of Scarlet Devil", tooltip: "06 - Koumakan", key: "EoSD" },
-      { name: "Perfect Cherry Blossom", tooltip: "07 - Youyoumu", key: "PCB" },
-      { name: "Immaterial and Missing Power", tooltip: "07.5 - Suimusou", key: "IaMP" },
-      { name: "Imperishable Night", tooltip: "08 - Eiyashou", key: "IN" },
-      { name: "Phantasmagoria of Flower View", tooltip: "09 - Kaeidzuka", key: "PoFV" },
-      { name: "Shoot the Bullet", tooltip: "09.5 - Bunkachou", key: "StB" },
-      { name: "Mountain of Faith", tooltip: "10 - Fuujinroku", key: "MoF" },
-      { name: "Scarlet Weather Rhapsody", tooltip: "10.5 - Hisouten", key: "SWR" },
-      { name: "Subterranean Animism", tooltip: "11 - Chireiden", key: "SA" },
-      { name: "Undefined Fantastic Object", tooltip: "12 - Seirensen", key: "UFO" },
-      { name: "Touhou Hisoutensoku", tooltip: "12.3 - Hisoutensoku", key: "soku" },
-      { name: "Double Spoiler", tooltip: "12.5 - Bunkachou", key: "DS" },
-      { name: "Great Fairy Wars", tooltip: "12.8 - Daisensou", key: "GFW" },
-      { name: "Ten Desires", tooltip: "13 - Shinreibyou", key: "TD" },
-      { name: "Hopeless Masquerade", tooltip: "13.5 - Shinkirou", key: "HM" },
-      { name: "Double Dealing Character", tooltip: "14 - Kishinjou", key: "DDC" },
-      { name: "Impossible Spell Card", tooltip: "14.3 - Amanojaku", key: "ISC" },
-      { name: "Urban Legend in Limbo", tooltip: "14.5 - Shinpiroku", key: "ULiL" },
-      { name: "Legacy of Lunatic Kingdom", tooltip: "15 - Kanjuden", key: "LoLK" },
-      { name: "Antinomy of Common Flowers", tooltip: "15.5 - Hyouibana", key: "AoCF" },
-      { name: "Hidden Star in Four Seasons", tooltip: "16 - Tenkuushou", key: "HSiFS" },
-      { name: "Violet Detector", tooltip: "16.5 - Hifuu Nightmare Diary", key: "VD" },
-      { name: "Wily Beast and Weakest Creature", tooltip: "17 - Kikeijuu", key: "WBaWC" },
-      { name: "Unconnected Marketeers", tooltip: "18 - Kouryuudou", key: "UM" },
+      { name: "Glacio",  key: "glacio" },
+      { name: "Fusion",  key: "fusion" },
+      { name: "Electro", key: "electro" },
+      { name: "Aero",    key: "aero" },
+      { name: "Spectro", key: "spectro" },
+      { name: "Havoc",   key: "havoc" },
     ]
   },
   {
-    name: "Filter by Stage Enemy Appearances",
-    key: "stage",
-    tooltip: "Check this to restrict to characters that appear in certain stages as enemies.",
+    name: "Filter by Weapon",
+    key: "weapon",
+    tooltip: "Check this to restrict to characters weapon.",
     checked: false,
-    sub: [ { name: "Stage 1", key: "st1" }, { name: "Stage 2", key: "st2" }, { name: "Stage 3", key: "st3" }, { name: "Stage 4", key: "st4" }, { name: "Stage 5/Penultimate", tooltip: "Stage 4 in 5-stage games, and Stage 8 in 9-stage games.", key: "st5" }, { name: "Stage 6/Final", key: "st6" }, { name: "Stage EX/Phantasm", key: "ex" }
+    sub: [
+      { name: "Sword",      key: "sword" },
+      { name: "Broadblade", key: "broadblade" },
+      { name: "Pistols",    key: "pistols" },
+      { name: "Gauntlets",  key: "gauntlets" },
+      { name: "Rectifier",  key: "rectifier" },
     ]
   },
   {
-    name: "Remove PC-98 Duplicates",
-    key: "pc98",
-    tooltip: "Check this to remove PC-98 characters with a Windows counterpart."
+    name: "Filter by Rarity",
+    key: "rarity",
+    tooltip: "Check this to restrict to characters rarity.",
+    checked: false,
+    sub: [
+      { name: "4⭐", key: "s4" },
+      { name: "5⭐", key: "s5" },
+    ]
   },
   {
-    name: "Remove Non-Girls",
-    key: "notgirl",
-    tooltip: "Check this to remove all non-female characters."
+    name: "Filter by the First Banner Version",
+    key: "version",
+    tooltip: "Check this to restrict to the version of chracters first banner.",
+    checked: false,
+    sub: [
+      { name: "1.0", key: "10" },
+      { name: "1.1", key: "11" },
+      { name: "1.2", key: "12" },
+      { name: "1.3", key: "13" },
+      { name: "1.4", key: "14" },
+    ]
   },
   {
-    name: "Remove Nameless Characters",
-    key: "nameless",
-    tooltip: "Check this to remove all characters without canonical names.",
-    checked: false
-  }
+    name: "Filter by Gender",
+    key: "gender",
+    tooltip: "Check this to restrict to characters gender.",
+    checked: false,
+    sub: [
+      { name: "Male", key: "male" },
+      { name: "Female", key: "female" },
+    ]
+  },
+  {
+    name: "Exclude Rover",
+    key: "rover",
+    tooltip: "Check this to not sorting with Rover.",
+    checked: true,
+  },
 ];
 
 dataSet[dataSetVersion].characterData = [
   {
-    name: "Sanhua",
-    img: "T_IconRoleHead256_7_UI.webp",
+    name: "Yangyang",
+    img: "T_IconRoleHead256_1_UI.webp",
     opts: {
-      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
-      stage: ["st4"]
+      type: ["aero"],
+      weapon: ["sword"],
+      rarity: ["s4"],
+      version: ["10"],
+      gender: ["female"],
+    }
+  },
+  {
+    name: "Chixia",
+    img: "T_IconRoleHead256_2_UI.webp",
+    opts: {
+      type: ["fusion"],
+      weapon: ["pistols"],
+      rarity: ["s4"],
+      version: ["10"],
+      gender: ["female"],
+    }
+  },
+  {
+    name: "Verina",
+    img: "T_IconRoleHead256_3_UI.webp",
+    opts: {
+      type: ["spectro"],
+      weapon: ["rectifier"],
+      rarity: ["s5"],
+      version: ["10"],
+      gender: ["female"],
+    }
+  },
+  {
+    name: "Male Rover",
+    img: "T_IconRoleHead256_4_UI.webp",
+    opts: {
+      type: ["spectro", "spectro", "aero"],
+      weapon: ["sword"],
+      rarity: ["s5"],
+      version: ["10"],
+      gender: ["female"],
+      rover: true
+    }
+  },
+  {
+    name: "Female Rover",
+    img: "T_IconRoleHead256_5_UI.webp",
+    opts: {
+      type: ["spectro", "spectro", "aero"],
+      weapon: ["sword"],
+      rarity: ["s5"],
+      version: ["10"],
+      gender: ["female"],
+      rover: true
     }
   },
   {
     name: "Baizhi",
     img: "T_IconRoleHead256_6_UI.webp",
     opts: {
-      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
-      stage: ["st4", "ex"]
+      type: ["glacio"],
+      weapon: ["rectifier"],
+      rarity: ["s4"],
+      version: ["10"],
+      gender: ["female"],
     }
-  }
+  },
+  {
+    name: "Sanhua",
+    img: "T_IconRoleHead256_7_UI.webp",
+    opts: {
+      type: ["glacio"],
+      weapon: ["sword"],
+      rarity: ["s4"],
+      version: ["10"],
+      gender: ["female"],
+    }
+  },
+  {
+    name: "Enchore",
+    img: "T_IconRoleHead256_8_UI.webp",
+    opts: {
+      type: ["fusion"],
+      weapon: ["rectifier"],
+      rarity: ["s5"],
+      version: ["10"],
+      gender: ["female"],
+    }
+  },
+  {
+    name: "Taoqi",
+    img: "T_IconRoleHead256_9_UI.webp",
+    opts: {
+      type: ["havoc"],
+      weapon: ["broadblade"],
+      rarity: ["s4"],
+      version: ["10"],
+      gender: ["female"],
+    }
+  },
+  {
+    name: "Danjin",
+    img: "T_IconRoleHead256_10_UI.webp",
+    opts: {
+      type: ["havoc"],
+      weapon: ["sword"],
+      rarity: ["s4"],
+      version: ["10"],
+      gender: ["female"],
+    }
+  },
+  {
+    name: "Jiyan",
+    img: "T_IconRoleHead256_11_UI.webp",
+    opts: {
+      type: ["aero"],
+      weapon: ["broadblade"],
+      rarity: ["s5"],
+      version: ["10"],
+      gender: ["male"],
+    }
+  },
+  {
+    name: "Aalto",
+    img: "T_IconRoleHead256_12_UI.webp",
+    opts: {
+      type: ["aero"],
+      weapon: ["psitols"],
+      rarity: ["s4"],
+      version: ["10"],
+      gender: ["male"],
+    }
+  },
+  {
+    name: "Mortefi",
+    img: "T_IconRoleHead256_13_UI.webp",
+    opts: {
+      type: ["fusion"],
+      weapon: ["psitols"],
+      rarity: ["s4"],
+      version: ["10"],
+      gender: ["male"],
+    }
+  },
+  {
+    name: "Lingyang",
+    img: "T_IconRoleHead256_14_UI.webp",
+    opts: {
+      type: ["glacio"],
+      weapon: ["gauntlets"],
+      rarity: ["s5"],
+      version: ["10"],
+      gender: ["male"],
+    }
+  },
+  {
+    name: "Yuanwu",
+    img: "T_IconRoleHead256_15_UI.webp",
+    opts: {
+      type: ["electro"],
+      weapon: ["gauntlets"],
+      rarity: ["s4"],
+      version: ["10"],
+      gender: ["male"],
+    }
+  },
+  {
+    name: "Yinlin",
+    img: "T_IconRoleHead256_17_UI.webp",
+    opts: {
+      type: ["electro"],
+      weapon: ["rectifier"],
+      rarity: ["s5"],
+      version: ["10"],
+      gender: ["female"],
+    }
+  },
+  {
+    name: "Calcharo",
+    img: "T_IconRoleHead256_18_UI.webp",
+    opts: {
+      type: ["electro"],
+      weapon: ["broadblade"],
+      rarity: ["s5"],
+      version: ["10"],
+      gender: ["male"],
+    }
+  },
+  {
+    name: "Jianxin",
+    img: "T_IconRoleHead256_23_UI.webp",
+    opts: {
+      type: ["aero"],
+      weapon: ["gauntlets"],
+      rarity: ["s5"],
+      version: ["10"],
+      gender: ["female"],
+    }
+  },
 ];
